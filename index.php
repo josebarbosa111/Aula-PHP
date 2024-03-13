ccba3615fd9880bb54f3ba8b72962268bbbbcef9
Jose Carlos <jose.barbosa111@etec.sp.gov.br>
2024-03-13 17:28:49 -0300

    Está é a primeiro aula de php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira Aula de PHP</title>
+</head>
+<body>
+
+<?php 
+
+$nome = "Jose Carlos";
+$idade = 34;
+$altura = 1.70;
+$peso = 67.10;
+
+var_dump($nome);
+echo "<br>";
+var_dump($idade);
+echo "<br>";
+var_dump($altura);
+echo "<br>";
+var_dump($peso);
+echo "<br>";
+
+echo "Meu nome é $nome ";
+echo "<br>";
+echo "Olá Mundo!";
+
+?>
+
+</body>
+</html>
